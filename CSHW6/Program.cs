8f8b4702e34c1d720aa35ca3602513be2f78f85e
Maslianka <nazar2131@ukr.net>
2018-09-09 18:03:16 +0300

    First Commit

--- CSHW6/Program.cs
+++ CSHW6/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,6 +11,54 @@ namespace CSHW6
     {
         static void Main(string[] args)
         {
-        }
+            #region In Main() method declare Dictionary PhoneBook for keeping pairs PersonName-PhoneNumber. 
+
+            Dictionary<string, string> phoneBook = new Dictionary<string, string>();
+
+            #endregion
+
+            #region From file "phones.txt" read 9 pairs into PhoneBook. Write only PhoneNumbers into file "Phones.txt".
+            
+            string[] parts;
+            StreamReader reader = new StreamReader("phones.txt");
+            string line;
+            while ((line = reader.ReadLine()) != null)
+            {
+                parts = line.Split(',');
+                phoneBook.Add(parts[0], parts[1]);
+                
+            }
+            using (StreamWriter sw = new StreamWriter("Phoness.txt", false, System.Text.Encoding.Default))
+            {
+                foreach (var current in phoneBook)
+                {
+                    sw.WriteLine(current.Value);
+                }
+            }
+            #endregion
+
+            #region Find and print phone number by the given name (name input from console)
+            string lookingFor = Console.ReadLine();
+            string myValue = phoneBook[lookingFor];
+            Console.WriteLine(myValue);
+            #endregion
+
+            #region Change all phone numbers, which are in format 80######### into new format +380#########. The result write into file "New.txt"
+            using (StreamWriter sw = new StreamWriter("New.txt", false, Encoding.Default))
+            {
+
+                foreach (string s in phoneBook.Values)
+                {
+                    if (s.Length < 13)
+                    {
+                        sw.WriteLine($"+3{s}");
+                    }
+                }
+            }
+            #endregion
+
+            Console.ReadKey();
+        }        
     }
 }
+
---
+++ CSHW6T2/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSHW6T2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int start = 1;
+            int end = 100;
+            int nums;
+            Console.WriteLine("Enter 10 numbers: \n");
+
+            try
+            {
+                for (int i = 0; i < 10; i++)
+                {
+                    Console.Write($"Enter number {i+1} in range ({start}; {end}): ");
+                    nums = ReadNumber(start, end);
+                }
+            }
+            catch (ArgumentOutOfRangeException)
+            {
+                Console.Error.WriteLine("\n-> Error! Number was out of the range ({0}; {1})!\n", start, end);
+            }
+            catch (Exception e)
+            {
+                Console.Error.WriteLine("\n-> {0} -> {1}\n", e.GetType(), e.Message);
+            }
+            Console.ReadKey();
+        }
+
+
+        static int ReadNumber(int start, int end)
+        {
+            int number = int.Parse(Console.ReadLine());
+
+            if (number <= start || number >= end);
+
+            return number;
+        }
+    }
+}
